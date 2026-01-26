a9c0414542d8e9d19a311f84b5f2e3f8e94437b9
An Phuoc Dao <153256952+AnPhuoc2410@users.noreply.github.com>
2026-01-26 13:45:51 +0700

    Use environment variable for socket URL

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = 'http://localhost:3001';
+const URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:3001';
 
 export const socket = io(URL, {
   transports: ['websocket', 'polling'],
